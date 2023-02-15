f44cb38e4800f9ebf2e8bf9c9d935ce8355ff10f
CptBoy <romansiebert0660@gmail.com>
2023-02-15 15:50:19 +0100

    fixed wordpress install

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,29 +1,27 @@
 #!/bin/sh
-sed -i '/\[mariadb\]/a log_error=/var/lib/mysql/mariadb_error.log' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a log_warnings=4' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a user=mysql' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a mysqld.sock=\/var\/run\/mysqld\/mysqld.sock' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a port=3306' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a basedir=/usr' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a datadir=/var/lib/mysql' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a bind-address=0.0.0.0' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a log_error=\/var\/lib\/mysql\/mariadb_error.log' /etc/my.cnf.d/mariadb-server.cnf
-sed -i 's/skip-networking/skip-networking=0/g' /etc/my.cnf.d/mariadb-server.cnf
-sed -i '/\[mariadb\]/a skip-bind-address' /etc/my.cnf.d/mariadb-server.cnf
-chown -R mysql:mysql /var/lib/mysql
-mkdir -p /auth_pam_tool_dir/auth_pam_tool
-mysql_install_db \
-        --user=mysql \
-        --datadir=/var/lib/mysql \
-        --basedir=/usr
-mariadbd-safe \
-        --user=mysql \
-        --datadir=/var/lib/mysql & sleep 2 \
-        && mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" \
-        && mysql -u root -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" \
-        && mysql -u root -e "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;" \
-        && mysql -u root -e "DROP USER ''@'%';" \
-        && mysql -u root -e "FLUSH PRIVILEGES;" \
-        && mysqladmin -u root password $MARIADB_ROOT_PASSWORD
-mysqladmin -u root password $MARIADB_ROOT_PASSWORD shutdown
+
+if [ ! -d /auth_pam_tool_dir/auth_pam_tool ]; then
+	sed -i '/\[mariadb\]/a log_error=/var/lib/mysql/mariadb_error.log' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a log_warnings=4' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a user=mysql' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a mysqld.sock=\/var\/run\/mysqld\/mysqld.sock' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a port=3306' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a basedir=/usr' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a datadir=/var/lib/mysql' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a bind-address=0.0.0.0' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a log_error=\/var\/lib\/mysql\/mariadb_error.log' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i 's/skip-networking/skip-networking=0/g' /etc/my.cnf.d/mariadb-server.cnf
+	sed -i '/\[mariadb\]/a skip-bind-address' /etc/my.cnf.d/mariadb-server.cnf
+	chown -R mysql:mysql /var/lib/mysql
+	mkdir -p /auth_pam_tool_dir/auth_pam_tool
+	mysql_install_db
+	mariadbd-safe & sleep 1 \
+		&& mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" \
+	        && mysql -u root -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" \
+		&& mysql -u root -e "GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$MARIADB_USER'@'%' WITH GRANT OPTION;" \
+		&& mysql -u root -e "DROP USER ''@'%';" \
+		&& mysql -u root -e "FLUSH PRIVILEGES;" \
+		&& mysqladmin -u root password $MARIADB_ROOT_PASSWORD
+	mysqladmin -u root password $MARIADB_ROOT_PASSWORD shutdown
+fi
 exec "$@"
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,13 +1,28 @@
 #!/bin/sh
 
-# downloading && extracting wordpress
-wget -P /var/www/html/ http://wordpress.org/latest.tar.gz
-tar -xzf /var/www/html/latest.tar.gz -C /var/www/html/
-rm /var/www/html/latest.tar.gz
+if [ ! -f /var/www/html/latest.tar.gz ]; then
 
-# configuring php-fpm
-sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g' /etc/php8/php-fpm.d/www.conf
-sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php8/php-fpm.conf
-sed -i 's/;error_log = log\/php8\/error.log/error_log = \/var\/www\/html\/wordpress_error.log/g' /etc/php8/php-fpm.conf
+	# downloading && extracting wordpress
+	wget -P /var/www/html/ http://wordpress.org/latest.tar.gz
+	tar -xzf /var/www/html/latest.tar.gz -C /var/www/html/
+	rm -f /var/www/html/wordpress/wp-config-sample.php
+	
+	# configuring php-fpm
+	sed -i 's/listen = 127.0.0.1:9000/listen = 9000/g' /etc/php8/php-fpm.d/www.conf
+	sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php8/php-fpm.conf
+	sed -i 's/;error_log = log\/php8\/error.log/error_log = \/var\/www\/html\/wordpress_error.log/g' /etc/php8/php-fpm.conf
+	
+	# installing wordpress
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /bin/wp
+	wp core install	--allow-root \
+			--path=/var/www/html/wordpress \
+			--url=${DOMAIN} \
+			--title=${WORDPRESS_TITLE} \
+			--admin_user=${WORDPRESS_ROOT_USER} \
+			--admin_password=${WORDPRESS_ROOT_PASSWORD} \
+			--admin_email=${WORDPRESS_ROOT_MAIL}
+fi
 
 exec "$@"
